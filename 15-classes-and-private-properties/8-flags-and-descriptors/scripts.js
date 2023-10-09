3958acaa36ca30676d1988e76ed36cea963371d9
Norwak <norwak99@gmail.com>
2023-10-09 12:07:58 +0500

    Class flags and descriptiors

---
+++ 15-classes-and-private-properties/8-flags-and-descriptors/scripts.js
@@ -0,0 +1,19 @@
+// [[Configurable]] - if `true`, the property can be deleted and these attributes can be modified, otherwise not
+// [[Enumerable]] - if `true`, the property will be returned in a `for...in` loop, otherwise not
+// [[Writable]] - if `true`, the value of the property can be changed, otherwise not
+// [[Value]] - the value of the property
+
+Math.PI = 4;
+console.log(Math.PI);
+
+let descriptor = Object.getOwnPropertyDescriptor(Math, 'PI');
+console.log(descriptor);
+
+const rectObj = {
+  name: 'Rectabgle 1',
+  width: 10,
+  height: 10
+};
+
+descriptor = Object.getOwnPropertyDescriptor(rectObj, 'name');
+console.log(descriptor);
